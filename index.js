f6a914c3ccee26e28b5cef343a8f9b8d10f1ecb0
KosM <t13droid@gmail.com>
2021-06-19 01:28:06 +0300

    catch some error if .env not exist in users dir
    eslint enabled airbnb

--- index.js
+++ index.js
@@ -1,50 +1,58 @@
 const dotenv = require('dotenv');
 const fs = require('fs');
-const watchFile = fs.watchFile
+
+const { watchFile } = fs;
 
 class envHotReloader {
-    constructor({ onEnvChange  , watchInterval }){
-        this.watchInterval = watchInterval || 3500; // 3.5 seconds
-        this.userExtraCB = onEnvChange || null
-        this.onChange = this.onChange.bind(this)
-        dotenv.config()
+  constructor({ onEnvChange, watchInterval }) {
+    this.watchInterval = watchInterval || 3500; // 3.5 seconds
+    this.userExtraCB = onEnvChange || null;
+    this.onChange = this.onChange.bind(this);
+    dotenv.config()
+  }
 
-    }
+  watch() {
+    this.AppendWarning();
+    watchFile(`${process.cwd()}/.env`, { interval: this.watchInterval }, this.onChange);
+  }
 
-    watch() {
-      this.AppendWarning()
-      watchFile(process.cwd()+ '/.env',{interval:this.watchInterval},   this.onChange );
-     }
-
-    onChange(curr , prev){
-    const result = dotenv.config()
+  onChange(curr, prev) {
+    const result = dotenv.config();
     if (result.error) {
-        throw result.error
+      console.error(`env-hot-reload: ${result.error.message}`);
+      return;
     }
-    const keys = Object.keys(result.parsed)
-           keys.forEach(x => {
-            let val = result.parsed[x]
-            console.log(`${x}:\x1b[32m${result.parsed[x]}\x1b[0m`)
-            process.env[x] = val
+    const keys = Object.keys(result.parsed);
+    keys.forEach((x) => {
+      const val = result.parsed[x];
+      console.log(`${x}:\x1b[32m${result.parsed[x]}\x1b[0m`);
+      process.env[x] = val;
+    });
+    console.log('.env reloaded!');
+    if (typeof this.userExtraCB === 'function') {
+      this.userExtraCB.call(this);
+    }
+  }
+
+  AppendWarning() {
+    try {
+      fs.readFile(`${__dirname}/warn.txt`, 'utf-8', (err, warnMesg) => {
+        fs.readFile(`${process.cwd()}/.env`, 'utf-8', (err2, userEnv) => {
+          const Env = `${warnMesg}\r\n${userEnv}`;
+          if (err2) {
+            console.error(`env-hot-reload: ${err2.message}`);
+            return;
+          }
+          const warnMesglines = warnMesg.split(/\r?\n/);
+          const userEnvlines = userEnv.split(/\r?\n/);
+          if (warnMesglines[0] === userEnvlines[0]) return;
+          console.warn(warnMesg);
+          fs.writeFile(`${process.cwd()}/.env`, Env, () => {});
         });
-      console.log('.env reloaded!')
-      if (typeof this.userExtraCB === 'function'){
-          this.userExtraCB.call(this)
-      }
+      });
+    } catch (e) {
+      console.log(e.message);
     }
-
-    AppendWarning(){
-        fs.readFile( __dirname+ '/warn.txt' , 'utf-8' , function(err,warnMesg){
-        fs.readFile(process.cwd() +'/.env' , 'utf-8' , function(err2,userEnv){
-        const Env = warnMesg + '\r\n' + userEnv;
-        const warnMesglines = warnMesg.split(/\r?\n/) 
-        const userEnvlines = userEnv.split(/\r?\n/) 
-        if (warnMesglines[0] === userEnvlines[0]) return
-        console.warn(warnMesg);
-        fs.writeFile(process.cwd() +'/.env' , Env , function(){});        
-       })
-   })
-    }
-
+  }
 }
-module.exports = envHotReloader
+module.exports = envHotReloader;
